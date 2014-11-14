8fb5aa101951fd16000f40845bfbdb4fd6a5fb24
thehexia <onmyexia@live.com>
2014-11-14 17:52:21 -0500

    Removed cascading deletes on models.

--- Kozol/Models/Invite.cs
+++ Kozol/Models/Invite.cs
@@ -10,21 +10,20 @@ namespace Kozol.Models
     public class Invite
     {
         [Key]
-        [Column(Order = 1)]
+        public int InviteId { get; set; }
+
+        [Required]
         public int SenderId { get; set; }
 
         [ForeignKey("SenderId")]
         public virtual UserProfile Sender { get; set; }
 
-        [Key]
-        [Column(Order = 2)]
+        [Required]
         public int ReceiverId { get; set; }
 
         [ForeignKey("ReceiverId")]
         public virtual UserProfile Receiver { get; set; }
 
-        [Key]
-        [Column(Order = 3)]
         public int ChannelId { get; set; }
 
         public virtual Channel Channel { get; set; }
--- Kozol/Models/KozolDbContext.cs
+++ Kozol/Models/KozolDbContext.cs
@@ -8,6 +8,11 @@ namespace Kozol.Models
 {
     public class KozolDbContext : DbContext
     {
+        public KozolDbContext()
+            : base("DefaultConnection")
+        {
+        }
+
         public DbSet<Channel> Channels { get; set; }
         public DbSet<Message> Messages { get; set; }
         public DbSet<Image> Images { get; set; }
@@ -15,5 +20,63 @@ namespace Kozol.Models
         public DbSet<Speaker> Speakers { get; set; }
         public DbSet<Friends> Friends { get; set; }
         public DbSet<Invite> Invites { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Message>()
+                        .HasRequired(a => a.Sender)
+                        .WithMany()
+                        .HasForeignKey(u => u.SenderId).WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<Message>()
+                        .HasRequired(a => a.Receiver)
+                        .WithMany()
+                        .HasForeignKey(u => u.ReceiverId).WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<Administrator>()
+                        .HasRequired(a => a.Channel)
+                        .WithMany()
+                        .HasForeignKey(u => u.ChannelId).WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<Administrator>()
+                        .HasRequired(a => a.User)
+                        .WithMany()
+                        .HasForeignKey(u => u.UserId).WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<Speaker>()
+                        .HasRequired(a => a.Channel)
+                        .WithMany()
+                        .HasForeignKey(u => u.ChannelId).WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<Speaker>()
+                        .HasRequired(a => a.User)
+                        .WithMany()
+                        .HasForeignKey(u => u.UserId).WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<Friends>()
+                        .HasRequired(a => a.Sender)
+                        .WithMany()
+                        .HasForeignKey(u => u.SenderId).WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<Friends>()
+                        .HasRequired(a => a.Receiver)
+                        .WithMany()
+                        .HasForeignKey(u => u.ReceiverId).WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<Invite>()
+                        .HasRequired(a => a.Channel)
+                        .WithMany()
+                        .HasForeignKey(u => u.ChannelId).WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<Invite>()
+                        .HasRequired(a => a.Sender)
+                        .WithMany()
+                        .HasForeignKey(u => u.SenderId).WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<Invite>()
+                        .HasRequired(a => a.Receiver)
+                        .WithMany()
+                        .HasForeignKey(u => u.ReceiverId).WillCascadeOnDelete(false);
+        }
     }
 }
