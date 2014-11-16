4ee6ab7f58c0a2d922b756b43a2aba491151cac8
thehexia <onmyexia@live.com>
2014-11-16 14:58:12 -0500

    Pending Actual Testing: Added first 4 Basic functions. Added Controllers for UserProfiles and Channels. Added some insert functions.

--- Kozol/Controllers/MessageController.cs
+++ Kozol/Controllers/MessageController.cs
@@ -3,32 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using WebMatrix.WebData;
 using Kozol.Models;
 
 namespace Kozol.Controllers
 {
-    public class ChannelController : Controller
+    public class MessageController : Controller
     {
         KozolDbContext db = new KozolDbContext();
         UsersContext users = new UsersContext();
 
-        public ActionResult Index()
-        {
-            var channels = from Channels in db.Channels
-                           select Channels;
-            return View(channels.ToList());
-        }
-
         [Authorize]
-        public EmptyResult AddChannel(Channel channel)
+        public void InsertMessage(Message message)
         {
             if (ModelState.IsValid)
             {
-                db.Channels.Add(channel);
+                db.Messages.Add(message);
                 db.SaveChanges();
             }
-            return new EmptyResult();
         }
     }
 }
---
+++ Kozol/Controllers/UsersController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Kozol.Models;
+
+namespace Kozol.Controllers
+{
+    public class UsersController : Controller
+    {
+        KozolDbContext db = new KozolDbContext();
+        UsersContext users = new UsersContext();
+
+        public ActionResult UserList()
+        {
+            var userList = from Users in users.UserProfiles
+                           select Users;
+            return View(userList.ToList());
+        }
+
+        public ActionResult UserEmailLookup(string username)
+        {
+            var email = (from Users in users.UserProfiles
+                         where Users.UserName.Contains(username)
+                         select Users).FirstOrDefault();
+
+            return View(email);
+        }
+
+        public ActionResult UserMessageHistory(string username)
+        {
+            var msgs = from Users in users.UserProfiles
+                       join Messages in db.Messages
+                       on Users.UserId equals Messages.SenderId
+                       where Users.UserName == username
+                       select Messages.Text;
+
+            return View(msgs.ToList());
+        }
+    }
+}
