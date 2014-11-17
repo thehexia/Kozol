06a3ff980a4626b31a01ae05a6ddcb3fbb8234d2
Roundaround <esteinkerchner@gmail.com>
2014-11-17 16:46:29 -0500

    More NuGet package updating, added basic SignalR startup code

--- Kozol/App_Start/RouteConfig.cs
+++ Kozol/App_Start/RouteConfig.cs
@@ -4,7 +4,10 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using Microsoft.Owin;
+using Owin;
 
+[assembly: OwinStartup(typeof(Kozol.Startup))]
 namespace Kozol
 {
     public class RouteConfig
@@ -18,6 +21,12 @@ namespace Kozol
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+        }
+    }
+
+    public class Startup {
+        public void Configuration(IAppBuilder app) {
+            app.MapSignalR();
         }
     }
 }
