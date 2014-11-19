3d81b3efaec87bac583a7115aae2aae00854ae10
Hoang Nguyen <thehexia@gmail.com>
2014-11-18 19:57:49 -0500

    Restarted ChannelController. Added some views for the sake of testing.

---
+++ Kozol/Controllers/ChannelController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Kozol.Models;
+using Kozol.Utilities;
+
+namespace Kozol.Controllers
+{
+    public class ChannelController : Controller
+    {
+        KozolContainer db = new KozolContainer();
+
+        public ActionResult ChannelIndex()
+        {
+            var channels = from channel in db.Channels
+                           select channel;
+
+            return View(channels.ToList());
+        }
+
+        public JsonResult ChannelList()
+        {
+            var channels = from channel in db.Channels
+                           select new ChannelViewModel
+                           {
+                               ID = channel.ID,
+                               Name = channel.Name,
+                               Created = channel.Created,
+                               Capacity = channel.Capacity,
+                               Mode_Admin = channel.Mode_Admin,
+                               Mode_Slow = channel.Mode_Slow,
+                               Mode_Quiet = channel.Mode_Quiet,
+                               Mode_Invite = channel.Mode_Invite
+                           };;
+
+            return Json(channels.ToList(), JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult AddChannel(Channel channel)
+        {
+            channel.Created = DateTime.Now;
+            if (Session["userId"] != null)
+            {
+                channel.Creator = db.Users.Find(Session["userId"]);
+            }
+            else
+            {
+                return Json(null, JsonRequestBehavior.AllowGet);
+            }
+
+            if (ModelState.IsValid)
+            {
+                db.Channels.Add(channel);
+                db.SaveChanges();
+            }
+            //need this because of circular reference caused by users
+            return Json(new { ID = channel.ID,
+                              //Creator = channel.Creator,
+                              Created = channel.Created,
+                              Capacity = channel.Capacity,
+                              Mode_Admin = channel.Mode_Admin,
+                              Mode_Slow = channel.Mode_Slow,
+                              Mode_Quiet = channel.Mode_Quiet, 
+                              Mode_Invite = channel.Mode_Invite
+            }, JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
---
+++ Kozol/Models/ViewModels/ChannelViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Kozol.Models
+{
+    public class ChannelViewModel
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public System.DateTime Created { get; set; }
+        public int Capacity { get; set; }
+        public bool Mode_Slow { get; set; }
+        public bool Mode_Admin { get; set; }
+        public bool Mode_Quiet { get; set; }
+        public bool Mode_Invite { get; set; }
+    }
+}
