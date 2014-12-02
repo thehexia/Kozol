ef425cb540d68f1a422eff7d5e8f8afa8f17b1d7
Hoang Nguyen <thehexia@gmail.com>
2014-12-02 08:24:09 -0500

    Inv view model

---
+++ Kozol/Models/ViewModels/InviteViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Kozol.Models.ViewModels
+{
+    public class InviteViewModel
+    {
+        public int SenderId { get; set; }
+        public int ReceieverId { get; set; }
+        public int ChannelId { get; set; }
+        public string ChannelName { get; set; }
+        public string SenderUsername { get; set; }
+        public string ReceiverUsername { get; set; }
+        public string SenderPublicKey { get; set; }
+        public string SharedKey { get; set; }
+        
+    }
+}
